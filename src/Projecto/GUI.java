d336b084a68c81f3695e495add68e242f8f62f4d
tiagosousa23 <120377095+tiagosousa23@users.noreply.github.com>
2022-12-12 17:29:56 +0000

    Criar janela da classe GUI e Painel de Base

--- src/Projecto/GUI.java
+++ src/Projecto/GUI.java
@@ -6,8 +6,14 @@ public class GUI extends JFrame {
     GUI() {
 
         //Criar janela, torná-la visível e determinar que programa pára de executar ao fechar a janela
-        JFrame janela = new JFrame("Empresa AoR-Autocarros");
-        janela.setVisible(true);
-        janela.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setTitle("Empresa AoR Autocarros");
+
+
+        setSize(450, 700);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        PainelDeBase p = new PainelDeBase();
+
+        getContentPane().add(p);
+        setVisible(true);
     }
 }
---
+++ src/Projecto/PainelDeBase.java
@@ -0,0 +1,16 @@
+package Projecto;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class PainelDeBase extends JPanel {
+
+    protected CardLayout layout;
+
+    PainelDeBase(){
+        setBackground(Color.green);
+
+    }
+
+}
+
